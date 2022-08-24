4f998ae996eef0968b837f5701e5b045b0594a31
Aleksey <smit_alexey@mail.ru>
2022-08-24 12:55:27 +0500

    Количество положительных элементов

---
+++ task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите через пробел M чисел: ");
+int[] massiv = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int j = 0;
+for (int i = 0; i < massiv.Length; i++)
+{
+    if (massiv[i] > 0)
+    {
+        j++;
+    }
+}
+Console.WriteLine($"Количество положительных элементов: {j}");
